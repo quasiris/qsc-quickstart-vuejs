27e756adb1670e0781cc049db21bc31340968f56
ShawalKhan786 <shawalkhnz@gmail.com>
2023-03-31 20:20:40 +0500

    commit

--- src/components/PageFacet.vue
+++ src/components/PageFacet.vue
@@ -2,18 +2,21 @@
   <h2>Our Products Range</h2>
   <v-card>
     <v-col cols="12" v-for="facet in facets" :key="facet">
-      <button>
+     
         <h2>{{ facet.name }}</h2>
-      </button>
+   
       <ul>
+      
+        
         <div class="filters" v-for="value in facet.values" :key="value">
-          <input class="checkbox"
+  <v-toolbar class="toolbar">        <input class="checkbox"
             type="checkbox"
             :value="value.filter"
             v-model="selectedFilters"
-          />
-          {{ value.value }}
+          /> &nbsp;
+         {{ value.value }} <v-spacer></v-spacer> {{value.count}} </v-toolbar>
         </div>
+      
       </ul>
     </v-col>
   </v-card>
@@ -46,10 +49,14 @@ export default {
 <style>
 .filters{
      font-size: 13px;
+     font-weight: bold;
 
 }
 .checkbox {
   transform: scale(0.9);
+}
+.toolbar{
+  background-color: white;
 }
 
 
