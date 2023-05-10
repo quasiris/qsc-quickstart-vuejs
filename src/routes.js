ba08c95d5dbeb6b01a80a78b8b5ad6127b3fbbbe
ShawalKhan786 <shawalkhnz@gmail.com>
2023-05-10 11:27:38 +0200

    commit

--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,16 @@
 import {createWebHashHistory, createRouter} from "vue-router";
+import Home from './components/ProductDetails.vue';
+
+const routes=[
+    {
+        name: 'ProductDetails',
+        path: '/',
+        component: ProductDetails
+    }
+];
+const router=createRouter({
+history:createWebHistory(),
+routes
+});
+
+export default router;
